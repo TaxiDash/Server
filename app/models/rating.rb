d24d4519849fddc0360cd5fec6f27f1cdd7a5c3d
Brian Broll <brian.broll@gmail.com>
2014-02-09 17:36:38 -0600

    Added "space" to acceptable char's in comments

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,7 +2,7 @@ class Rating < ActiveRecord::Base
     belongs_to :driver
     validates :rating, format: { with: /[12345]/, 
                                  message: "Must be integer 1-5" }
-    COMMENT_FORMAT = /[a-zA-Z.!?\d$"()]*/
+    COMMENT_FORMAT = /[a-zA-Z. !?\d$"()]*/
     validates :comments, length: { maximum: 250 ,
                                    message: "must be 250 characters or fewer." } 
     validates :comments, format: { with: Regexp.new('\A' + COMMENT_FORMAT.source + '\z'), 
