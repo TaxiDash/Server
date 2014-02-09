1f5dfcc63d8ce52de1d4347c0bc84e0fd45fbc4e
Brian Broll <brian.broll@gmail.com>
2014-02-09 17:30:44 -0600

    Removed demo route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ TaxiRatingServer::Application.routes.draw do
 
   resources :drivers
 
-  get "drivers_controller/demo"
     #root :to => 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
