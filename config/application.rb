36996cc0220971378ca68a9302c975f484b37e66
Seth Friedman <sethfri@gmail.com>
2014-02-19 19:47:32 -0600

    Do not initialize assets on precompile

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module TaxiRatingServer
     # config.i18n.default_locale = :de
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    config.assets.initialize_on_precompile = false
   end
 end
