6cde7f8bdedaabc0521d32b7a0b79476e4351f79
Brian Broll <brian.broll@vanderbilt.edu>
2014-03-29 09:49:09 -0500

    Removed vestigial code (edit)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,10 +7,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
         authorize! :create, resource
     end
 
-    def edit
-        puts @current_user.id.to_s << " (" << @current_user.username << ")"<< " is editing " << @user.id.to_s << " (" << @user.username << ")"
-        if @current_user.id == @user or can? :update, User then
-            super
-        end
-    end
 end
