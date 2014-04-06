cbac1107a20f79eebdd8226ba5233178d2d0092e
Brian Broll <brian.broll@gmail.com>
2014-04-06 18:19:43 -0500

    Changed rider_id to string so it can temporarily be uuid.

--- db/migrate/20140206021513_create_ratings.rb
+++ db/migrate/20140206021513_create_ratings.rb
@@ -2,7 +2,7 @@ class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
       t.integer :driver_id
-      t.integer :rider_id
+      t.string :rider_id
       t.integer :rating
       t.string :comments
       t.timestamp :timestamp
