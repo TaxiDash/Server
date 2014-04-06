4699c0b7c7ccd17d236d4f7c5eb7e6834ce60fb2
Brian Broll <brian.broll@gmail.com>
2014-04-06 18:21:47 -0500

    Added support for basic fraud detection.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,7 @@
 class Document < ActiveRecord::Base
     belongs_to :driver
     has_attached_file :doc, 
-        :path => ":rails_root/public/images/:class/:id/:basename_:style.:extension",
+        :path => ":rails_root/public/documents/:class/:id/:basename_:style.:extension",
         :url => "/documents/:class/:id/:basename_:style.:extension"
 
     validates_attachment :doc,
