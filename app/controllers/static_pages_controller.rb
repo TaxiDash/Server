51df0a9c7f74e8b115c065173b14df8a539054c7
Brian Broll <brian.broll@vanderbilt.edu>
2014-04-08 09:04:47 -0500

    Added overview entry

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
-    def import_export
-        authorize! :import_export, Driver
-        authorize! :import_export, Company
-        authorize! :import_export, Rider
-    end
+
+  def overview
+    #TODO Set highest rated drivers
+  end
+
+  def import_export
+    authorize! :import_export, Driver
+    authorize! :import_export, Company
+    authorize! :import_export, Rider
+  end
 end
