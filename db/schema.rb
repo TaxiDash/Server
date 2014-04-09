73de96142d41452d2c552843c44e197eaca36853
Emily Crowe <emily.v.crowe@vanderbilt.edu>
2014-04-08 20:29:27 -0500

    Refactored index code. Clickable rows...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140222014340) do
+ActiveRecord::Schema.define(version: 20140329003500) do
 
   create_table "drivers", force: true do |t|
     t.string   "first_name"
@@ -42,6 +42,10 @@ ActiveRecord::Schema.define(version: 20140222014340) do
     t.integer  "total_ratings",            default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "ratings", force: true do |t|
