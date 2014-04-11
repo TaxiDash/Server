2ccc52a1a14edd2b12cd23e5ca7cfc5352d1e372
Brian Broll <brian.broll@vanderbilt.edu>
2014-04-10 19:30:16 -0500

    Added latitude/longitude

--- db/migrate/20140409230520_create_rides.rb
+++ db/migrate/20140409230520_create_rides.rb
@@ -3,8 +3,10 @@ class CreateRides < ActiveRecord::Migration
     create_table :rides do |t|
       t.integer :driver_id
       t.integer :rider_id
-      t.float :start, :limit => 53, :null => true
-      t.float :end, :limit => 53, :null => true
+      t.float :start_latitude, :limit => 53, :null => true
+      t.float :start_longitude, :limit => 53, :null => true
+      t.float :end_latitude, :limit => 53, :null => true
+      t.float :end_longitude, :limit => 53, :null => true
       t.decimal :estimated_fare
       t.decimal :actual_fare
 
