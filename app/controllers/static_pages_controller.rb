c3132034f893b3519608cd74d592df87164bf9a8
Brian Broll <brian.broll@gmail.com>
2014-04-20 12:31:10 -0500

    Fixed authentication for static_pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!
 
   def overview
   end
 
   def get_ratings
-      :authenticate_user!
       length = params[:length].to_i || 5 #Entries to return
       sort_dir = params[:sort_dir] || "best" #Entries to return
 
