54cf0b4de0f212a38862ad9bde0616ed3b1aa50e
Brian Broll <brian.broll@vanderbilt.edu>
2014-05-11 10:51:46 -0500

    Added company phone numbers

--- db/migrate/20140330195637_create_companies.rb
+++ db/migrate/20140330195637_create_companies.rb
@@ -3,6 +3,7 @@ class CreateCompanies < ActiveRecord::Migration
     create_table :companies do |t|
       t.string :name
       t.attachment :logo
+      t.string :phone_number
       t.decimal :average_rating, :default => 0
       t.integer :total_ratings, :default => 0
 
